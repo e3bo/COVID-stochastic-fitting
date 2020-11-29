d86d863bb0437fa093cb892ad9bc7ba1f751e7ab
Eamon O'Dea <odea35@gmail.com>
2020-11-29 12:37:48 -0500

    Add ensemble to forecast

--- local-run/evaluate-forecasts.R
+++ local-run/evaluate-forecasts.R
@@ -8,16 +8,20 @@ rwf <- evalcast::get_covidhub_predictions("CEID-Walk",
                                           forecast_dates = "2020-11-16")
 utf <- evalcast::get_covidhub_predictions("UT-Mobility",
                                           forecast_dates = "2020-11-16")
+cef <- evalcast::get_covidhub_predictions("COVIDhub-ensemble",
+                                          forecast_dates = "2020-11-16")
+scef <- filter_predictions(cef, geo_type = "state")
 srwf <- filter_predictions(rwf, geo_type = "state")
 
 # case forecast evaluations
 esqs <- evalcast::evaluate_predictions(sqs[1], backfill_buffer = 0)
 elis <- evalcast::evaluate_predictions(lis[1], backfill_buffer = 0)
 erns <- evalcast::evaluate_predictions(rns[1], backfill_buffer = 0)
+scef <- evaluate_predictions(scef[1], backfill_buffer = 0)
 erwf <- evaluate_predictions(srwf[1], backfill_buffer = 0)
 
 
-ei <- evalcast:::intersect_locations(c(erwf, esqs, elis, erns))
+ei <- evalcast:::intersect_locations(c(erwf, esqs, elis, erns, scef))
 plot_measure(ei, "ae")
 plot_measure(ei, "wis")
 plot_width(ei)
@@ -25,6 +29,7 @@ plot_calibration(ei[[1]])
 plot_calibration(ei[[2]])
 plot_calibration(ei[[3]])
 plot_calibration(ei[[4]])
+plot_calibration(ei[[5]])
 
 # death forecast evaluations
 
@@ -33,8 +38,9 @@ elis2 <- evalcast::evaluate_predictions(lis[2], backfill_buffer = 0)
 erns2 <- evalcast::evaluate_predictions(rns[2], backfill_buffer = 0)
 erwf2 <- evaluate_predictions(srwf[2], backfill_buffer = 0)
 eutf <- evaluate_predictions(utf[1], backfill_buffer = 0)
+scef2 <- evaluate_predictions(scef[2], backfill_buffer = 0)
 
-ei2 <- evalcast:::intersect_locations(c(erwf2, esqs2, elis2, erns2, eutf))
+ei2 <- evalcast:::intersect_locations(c(erwf2, esqs2, elis2, erns2, eutf, scef2))
 plot_measure(ei2, "ae")
 plot_measure(ei2, "wis")
 plot_width(ei2)
@@ -43,3 +49,4 @@ plot_calibration(ei2[[2]])
 plot_calibration(ei2[[3]])
 plot_calibration(ei2[[4]])
 plot_calibration(ei2[[5]])
+plot_calibration(ei2[[6]])
