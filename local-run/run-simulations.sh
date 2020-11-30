3f3972d2e8faec9e6b3b0eddd0d5b7a4f794691a
Eamon O'Dea <odea35@gmail.com>
2020-11-29 21:32:18 -0500

    Add most states

--- local-run/run-simulations.sh
+++ local-run/run-simulations.sh
@@ -3,9 +3,23 @@
 set -e
 
 
-# Problem states: Florida
+# Problem states: Florida Massachusetts Lousianna "Nebraska" "New Jersey" Washington
 fdt="${fdt:-2020-11-16}"
 declare -a locs=(
+"Wisconsin"
+"West Virginia"
+"Wyoming"
+  "Missouri"
+  "Minnesota"
+  "Michigan"
+  "Maine"
+  "Maryland"
+  "Kentucky"
+  "Kansas"
+  "Indiana"
+  "Illinois"
+  "Idaho"
+  "Iowa"
   "Hawaii"
   "Delaware"
   "Connecticut"
@@ -15,9 +29,28 @@ declare -a locs=(
   "Alaska"
   "Alabama"
   "Arkansas"
-  "Minnesota" 
   "Georgia"
-                )
+  "Mississippi"
+"Montana"
+"North Carolina"
+"North Dakota"
+"New Hampshire"
+"New Mexico"
+"Nevada"
+"New York"
+"Ohio"
+"Oklahoma"
+"Oregon"
+"Pennsylvania"
+"Rhode Island"
+"South Carolina"
+"South Dakota"
+"Tennessee"
+"Texas"
+"Utah"
+"Virginia"
+"Vermont"
+)
                 
 for l in "${locs[@]}"; do
   fdt=$fdt locname=$l ./weekly-forecast.R
